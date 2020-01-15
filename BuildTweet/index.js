958ed8ff132106de2719e45085fe34913639ec4b
siawokoy <similola-a@hotmail.com>
2020-01-15 19:30:24 +0000

    updating code with unique tweet

--- BuildTweet/index.js
+++ BuildTweet/index.js
@@ -1,13 +1,12 @@
 module.exports = async function (context, req) {
-    let currentdate = new Date(); 
-    let datetime = currentdate.getDate() + "/"
-                    + (currentdate.getMonth()+1)  + "/" 
-                    + currentdate.getFullYear() + " at "  
-                    + currentdate.getHours() + ":"  
-                    + currentdate.getMinutes();
-    console.log(datetime);
+    let currentdate = new Date();
+
     context.log('Generating Tweet...');
     context.res = {
-        body: "A tweet a day keeps the doctor away by TweetDoctor bot on "+datetime
+        body: "A tweet a day keeps the doctor away by TweetDoctor bot on " + currentdate.getDate() + "/"
+            + (currentdate.getMonth() + 1) + "/"
+            + currentdate.getFullYear() + " at "
+            + currentdate.getHours() + ":"
+            + currentdate.getMinutes()
     };
 };
